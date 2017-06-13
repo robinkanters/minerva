c5c4fde678ee8d9baca89120c136c0383c7e514b
Robin Kanters <robin@robinkanters.nl>
2017-06-13 15:23:21 +0200

    Added parallel capability to forEach component (on by default)

--- src/main/kotlin/com/robinkanters/minerva/component/ForEachComponent.kt
+++ src/main/kotlin/com/robinkanters/minerva/component/ForEachComponent.kt
@@ -3,12 +3,19 @@ package com.robinkanters.minerva.component
 import com.robinkanters.minerva.Flow
 import com.robinkanters.minerva.Flow.Companion.flow
 import com.robinkanters.minerva.FlowComponent
+import java.util.stream.Stream
+import kotlin.streams.toList
 
-class ForEachComponent<T>(val flow: Flow<T>) : FlowComponent<List<T>> {
-    override fun run(payload: List<T>) = payload.map { flow.run(it) }
+class ForEachComponent<T>(val parallel: Boolean = true, val flow: Flow<T>) : FlowComponent<List<T>> {
+    override fun run(payload: List<T>) = payload.makeStream().map {
+        flow.run(it)
+    }.toList()
+
+    fun List<T>.makeStream(): Stream<T> = if (parallel) parallelStream() else stream()
 
     companion object {
         @JvmStatic
-        fun <T> Flow<List<T>>.forEach(subflow: Flow<T>.() -> Unit) = put(ForEachComponent(flow("", subflow)))
+        fun <T> Flow<List<T>>.forEach(parallel: Boolean = true, subflow: Flow<T>.() -> Unit)
+                = put(ForEachComponent(parallel, flow("", subflow)))
     }
 }
